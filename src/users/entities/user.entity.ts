317f3fca51604ed29e0b69ed02cd836a244480d3
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:30:00 +0530

    feat: user.entity.ts

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Task } from '../../tasks/entities/task.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'first_name' })
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: 'created_at', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ name: 'updated_at', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  // Relations
+  @OneToMany(() => Task, task => task.user)
+  tasks: Task[];
+}

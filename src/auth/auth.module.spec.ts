19f3e8c13dc1d805d3c505235b2c574172de708e
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:05:38 +0530

    feat: auth.module.spec.ts

---
+++ src/auth/auth.module.spec.ts
@@ -0,0 +1,17 @@
+// src/auth/auth.module.spec.ts
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthModule } from './auth.module';
+
+describe('AuthModule', () => {
+  let module: TestingModule;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [AuthModule],
+    }).compile();
+  });
+
+  it('should be defined', () => {
+    expect(module).toBeDefined();
+  });
+});

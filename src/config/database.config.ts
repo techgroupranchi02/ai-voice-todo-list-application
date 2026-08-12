a3176b16d7354486511c8e7c3a872082faede650
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:30:29 +0530

    feat: database.config.ts

---
+++ src/config/database.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'ai_voice_todo',
+  entities: [
+    __dirname + '/../**/*.entity{.ts,.js}',
+  ],
+  synchronize: true,
+  logging: false,
+};

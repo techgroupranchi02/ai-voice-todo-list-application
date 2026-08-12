84e8ba39d1d58427f207cb58fa65f84757d51c5e
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 17:42:14 +0530

    fix: resolve TypeORM driver and entity metadata errors in src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
-import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(),
-    DatabaseModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', '127.0.0.1'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USER', configService.get<string>('DB_USERNAME', 'postgres')),
+        password: configService.get<string>('DB_PASSWORD', 'password'),
+        database: configService.get<string>('DB_NAME', 'ai_voice_todo'),
+        entities: [User, Task],
+        synchronize: true,
+      }),
+    }),
     AuthModule,
     TasksModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],

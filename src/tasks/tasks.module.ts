9e09b1deda9e7393cb84386e549664ed17140b6f
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:29:19 +0530

    feat: tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,8 @@
-// src/tasks/tasks.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { Task } from './task.entity';
+import { Task } from './entities/task.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],

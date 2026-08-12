7a11bb4df3bc9d7c9ba97369126c7bc0332beb30
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:05:36 +0530

    feat: auth.guard.ts

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,11 @@
+// src/auth/auth.guard.ts
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    // Add your custom authentication logic here
+    return super.canActivate(context);
+  }
+}

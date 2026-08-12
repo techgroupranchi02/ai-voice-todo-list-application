26d6d5b177c91120eb890cecc2534a33dcb08dd6
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:29:51 +0530

    feat: users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,8 @@
-// src/users/users.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { User } from './user.entity';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],

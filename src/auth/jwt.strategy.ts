3b587ebefdc88b247c71c04deb63b34a06aba1e7
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:05:52 +0530

    feat: jwt.strategy.ts

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+// src/auth/jwt.strategy.ts
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || 'fallback_jwt_secret_key',
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.id, email: payload.email };
+  }
+}

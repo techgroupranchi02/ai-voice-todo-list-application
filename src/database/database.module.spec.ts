0f71b9818831e9dd4687012698fcf0a02da5a759
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:06:12 +0530

    feat: database.module.spec.ts

---
+++ src/database/database.module.spec.ts
@@ -0,0 +1,17 @@
+// src/database/database.module.spec.ts
+import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from './database.module';
+
+describe('DatabaseModule', () => {
+  let module: TestingModule;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule],
+    }).compile();
+  });
+
+  it('should be defined', () => {
+    expect(module).toBeDefined();
+  });
+});

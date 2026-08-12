b70641d91b8908b10e36216b16c7d85c1ff63c51
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:39:40 +0530

    feat: create-voice-task.dto.ts

--- src/tasks/dto/create-voice-task.dto.ts
+++ src/tasks/dto/create-voice-task.dto.ts
@@ -1,8 +1,7 @@
-// src/tasks/dto/create-voice-task.dto.ts
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateVoiceTaskDto {
-  @IsNotEmpty()
   @IsString()
+  @IsNotEmpty()
   audioData: string;
 }

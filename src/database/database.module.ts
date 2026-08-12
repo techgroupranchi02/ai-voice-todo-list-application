c150b52613b5f31a1f75dab73f38c198ce5895f8
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-12 10:35:06 +0530

    feat: database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,22 +1,7 @@
-// src/database/database.module.ts
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseProviders } from './database.providers';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'ai_voice_todo',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
-      logging: false,
-    }),
-  ],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })

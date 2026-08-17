240266cf2e93bc7558f39703302acafe72f29024
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:56:08 +0530

    feat: tasks.service.ts

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Task } from '../entities/task.entity';
+import { Task } from './entities/task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
@@ -11,16 +11,33 @@ export class TasksService {
     private tasksRepository: Repository<Task>,
   ) {}
 
-  async create(createTaskDto: CreateTaskDto, userId: number): Promise<Task> {
+  async create(createTaskDto: CreateTaskDto, userId: number): Promise<any> {
     const task = this.tasksRepository.create({
       ...createTaskDto,
       user: { id: userId },
-      completed: false,
     });
-    return await this.tasksRepository.save(task);
+    
+    const savedTask = await this.tasksRepository.save(task);
+    
+    return {
+      success: true,
+      data: {
+        taskId: savedTask.id,
+        message: 'Task created successfully.',
+      },
+    };
   }
 
   async findOne(id: number): Promise<Task> {
-    return await this.tasksRepository.findOne({ where: { id } });
+    const task = await this.tasksRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+    
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+    
+    return task;
   }
 }

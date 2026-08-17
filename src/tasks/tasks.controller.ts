5fc752862c37a1be0fbecb535e90e2805c87c52d
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:56:02 +0530

    feat: tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,21 +3,14 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller('api/v1/tasks')
+@Controller('tasks')
+@UseGuards(AuthGuard('jwt'))
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  @UseGuards(AuthGuard('jwt'))
   async create(@Body() createTaskDto: CreateTaskDto, @Request() req) {
     const userId = req.user.id;
-    const task = await this.tasksService.create(createTaskDto, userId);
-    return {
-      success: true,
-      data: {
-        taskId: task.id,
-        message: 'Task created successfully.',
-      },
-    };
+    return this.tasksService.create(createTaskDto, userId);
   }
 }

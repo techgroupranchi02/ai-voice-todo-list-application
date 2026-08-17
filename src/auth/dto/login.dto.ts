438f65ed87041b637f86a4b681be6b67a82dc2a6
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 13:54:21 +0530

    feat: login.dto.ts

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @IsString()
   @IsNotEmpty()
   password: string;
 }

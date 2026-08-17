5a25726b9665e3399b51b7daaa614c202257ec9c
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:42:16 +0530

    feat: user.repository.ts

--- src/auth/repositories/user.repository.ts
+++ src/auth/repositories/user.repository.ts
@@ -1,27 +1,28 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { ConflictException, NotFoundException } from '@nestjs/common';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const { email, firstName, lastName, password } = createUserDto;
+    const { firstName, lastName, email, password } = createUserDto;
+
+    const user = new User();
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.password = await bcrypt.hash(password, 10);
 
     try {
-      const user = this.create({
-        email,
-        firstName,
-        lastName,
-        password,
-      });
-
-      return await this.save(user);
+      return await user.save();
     } catch (error) {
       if (error.code === '23505') {
-        throw new ConflictException('An account with this email already exists.');
+        throw new ConflictException('Email already exists');
+      } else {
+        throw new InternalServerErrorException('Error creating user');
       }
-      throw error;
     }
   }
 
@@ -31,24 +32,5 @@ export class UserRepository extends Repository<User> {
 
   async findUserById(id: number): Promise<User> {
     return await this.findOne({ where: { id } });
-  }
-
-  async updateUser(id: number, updateData: Partial<User>): Promise<User> {
-    const user = await this.findUserById(id);
-    
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-    
-    Object.assign(user, updateData);
-    return await this.save(user);
-  }
-
-  async deleteUser(id: number): Promise<void> {
-    const result = await this.delete(id);
-    
-    if (result.affected === 0) {
-      throw new NotFoundException('User not found');
-    }
   }
 }

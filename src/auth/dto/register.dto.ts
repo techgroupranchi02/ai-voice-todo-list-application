b49cadbcae074546154e9a31a94695cbe3198c47
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:44:43 +0530

    feat: register.dto.ts

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, MinLength, MaxLength, IsOptional } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
@@ -7,15 +7,11 @@ export class RegisterDto {
 
   @IsNotEmpty()
   @MinLength(6)
-  @MaxLength(255)
   password: string;
 
   @IsNotEmpty()
-  @MinLength(1)
-  @MaxLength(255)
   firstName: string;
 
   @IsOptional()
-  @MaxLength(255)
-  lastName: string;
+  lastName?: string;
 }

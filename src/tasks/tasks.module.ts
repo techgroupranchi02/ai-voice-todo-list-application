3c07dc03549da86b5061c4a3f5f1b1791973c3f9
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 12:10:56 +0530

    feat: tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,19 +1,12 @@
+// src/tasks/tasks.module.ts
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
-import { Category } from './entities/category.entity';
-
-import { AuthModule } from '../auth/auth.module';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Task, Category]),
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
   providers: [TasksService],
   exports: [TasksService],

a2eba1e393772b22309e168b03e2a213c750e850
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 13:53:58 +0530

    feat: validation.pipe.ts

---
+++ src/common/pipes/validation.pipe.ts
@@ -0,0 +1,42 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata): Promise<any> {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToInstance(metatype, value);
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      const messages = errors.map((error) => 
+        Object.values(error.constraints).join(', ')
+      );
+      
+      throw new BadRequestException({
+        success: false,
+        error: {
+          status: 400,
+          message: messages.join('. '),
+          timestamp: new Date().toISOString(),
+        },
+      });
+    }
+
+    return value;
+  }
+
+  private toValidate(metatype: any): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.find((type) => metatype === type);
+  }
+}

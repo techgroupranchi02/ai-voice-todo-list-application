9339e0a03631bd6594d796b939f623a0ec266d36
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 15:07:05 +0530

    feat: category.repository.ts

--- src/tasks/repositories/category.repository.ts
+++ src/tasks/repositories/category.repository.ts
@@ -1,11 +1,9 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Category } from '../entities/category.entity';
-import { Injectable } from '@nestjs/common';
 
-@Injectable()
 @EntityRepository(Category)
 export class CategoryRepository extends Repository<Category> {
   // Custom repository methods can be added here
-  // For now, we're extending the base Repository with no additional methods
-  // as CRUD operations are inherited from Repository base class
+  // For now, we're extending the base Repository class
+  // which provides all standard CRUD operations
 }

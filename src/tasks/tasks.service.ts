15ee61c96151563bf5553f7d6b691d0f388f4395
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:45:14 +0530

    feat: tasks.service.ts

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,67 +1,26 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { TaskRepository } from './repositories/task.repository';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from '../entities/task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
-import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly taskRepository: TaskRepository) {}
+  constructor(
+    @InjectRepository(Task)
+    private tasksRepository: Repository<Task>,
+  ) {}
 
-  async create(createTaskDto: CreateTaskDto): Promise<Task> {
-    try {
-      const task = await this.taskRepository.createTask(createTaskDto);
-      return task;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async findAll(): Promise<Task[]> {
-    try {
-      return await this.taskRepository.findAllTasks();
-    } catch (error) {
-      throw error;
-    }
+  async create(createTaskDto: CreateTaskDto, userId: number): Promise<Task> {
+    const task = this.tasksRepository.create({
+      ...createTaskDto,
+      user: { id: userId },
+      completed: false,
+    });
+    return await this.tasksRepository.save(task);
   }
 
   async findOne(id: number): Promise<Task> {
-    try {
-      const task = await this.taskRepository.findTaskById(id);
-      
-      if (!task) {
-        throw new NotFoundException('Task not found');
-      }
-      
-      return task;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
-    try {
-      const task = await this.taskRepository.updateTask(id, updateTaskDto);
-      return task;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async remove(id: number): Promise<void> {
-    try {
-      await this.taskRepository.deleteTask(id);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async toggleCompletion(id: number): Promise<Task> {
-    try {
-      const task = await this.taskRepository.toggleTaskCompletion(id);
-      return task;
-    } catch (error) {
-      throw error;
-    }
+    return await this.tasksRepository.findOne({ where: { id } });
   }
 }

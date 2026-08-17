2a60e0b85eda32e6535512c1fc344df5d61ac781
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:46:02 +0530

    feat: team.entity.ts

--- src/teams/entities/team.entity.ts
+++ src/teams/entities/team.entity.ts
@@ -1,34 +1,25 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 
 @Entity('teams')
 export class Team {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-  @Column({ type: 'varchar', length: 255, nullable: false })
+  @Column({ length: 255 })
   name: string;
 
-  @Column({ type: 'text', nullable: true })
-  description: string;
-
-  @CreateDateColumn({ type: 'timestamp' })
-  createdAt: Date;
-
-  @UpdateDateColumn({ type: 'timestamp' })
-  updatedAt: Date;
-
-  @ManyToMany(() => User, user => user.teams)
+  @ManyToMany(() => User)
   @JoinTable({
     name: 'team_members',
     joinColumn: {
       name: 'team_id',
-      referencedColumnName: 'id'
+      referencedColumnName: 'id',
     },
     inverseJoinColumn: {
       name: 'user_id',
-      referencedColumnName: 'id'
-    }
+      referencedColumnName: 'id',
+    },
   })
   members: User[];
 }

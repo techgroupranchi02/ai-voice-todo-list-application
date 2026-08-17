aa719ab109ca18bf79735d2f23b48a5f97700b2e
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:43:00 +0530

    feat: category.repository.spec.ts

---
+++ src/tasks/repositories/category.repository.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryRepository } from './category.repository';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Category } from '../entities/category.entity';
+
+describe('CategoryRepository', () => {
+  let repository: CategoryRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CategoryRepository,
+        {
+          provide: getRepositoryToken(Category),
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    repository = module.get<CategoryRepository>(CategoryRepository);
+  });
+
+  it('should be defined', () => {
+    expect(repository).toBeDefined();
+  });
+
+  it('should extend Repository class', () => {
+    expect(repository).toBeInstanceOf(CategoryRepository);
+  });
+});

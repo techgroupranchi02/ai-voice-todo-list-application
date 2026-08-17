b1acf95128a9c4bdb856ccecdebc35d45dd1de9e
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-17 14:01:03 +0530

    feat: voice.module.ts

--- src/voice/voice.module.ts
+++ src/voice/voice.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from '../tasks/entities/task.entity';
 import { VoiceService } from './voice.service';
 import { VoiceController } from './voice.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from '../tasks/entities/task.entity';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task, User])],
   controllers: [VoiceController],
   providers: [VoiceService],
-  exports: [VoiceService],
 })
 export class VoiceModule {}

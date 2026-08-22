81d38045ea91dc89baa03425cd439e992781cbf2
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-22 11:34:01 +0530

    feat: team.entity.ts

--- src/teams/entities/team.entity.ts
+++ src/teams/entities/team.entity.ts
@@ -1,25 +1,32 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm';
-import { User } from '../../users/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { User } from '../../auth/entities/user.entity';
 
 @Entity('teams')
 export class Team {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column({ length: 255 })
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  @IsNotEmpty()
   name: string;
 
-  @ManyToMany(() => User)
-  @JoinTable({
-    name: 'team_members',
-    joinColumn: {
-      name: 'team_id',
-      referencedColumnName: 'id',
-    },
-    inverseJoinColumn: {
-      name: 'user_id',
-      referencedColumnName: 'id',
-    },
-  })
-  members: User[];
+  @Column({ type: 'text', nullable: true })
+  @IsOptional()
+  description: string;
+
+  @Column({ type: 'bigint', nullable: false })
+  @IsNotEmpty()
+  ownerId: number;
+
+  @CreateDateColumn({ type: 'timestamp', nullable: false })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', nullable: false })
+  updatedAt: Date;
+
+  // Relations
+  @ManyToOne(() => User, user => user.id)
+  @JoinColumn({ name: 'owner_id' })
+  owner: User;
 }

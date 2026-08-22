a0b735a89b491d859572308ced2e5fb2df6cdea0
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-22 10:28:00 +0530

    feat: teams.module.ts

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Team } from './entities/team.entity';
 import { TeamsService } from './teams.service';
 import { TeamsController } from './teams.controller';
-import { TeamRepository } from './repositories/team.repository';
+import { Task } from '../tasks/entities/task.entity';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Team])],
+  imports: [
+    TypeOrmModule.forFeature([Task, User]),
+  ],
+  providers: [TeamsService],
   controllers: [TeamsController],
-  providers: [TeamsService, TeamRepository],
   exports: [TeamsService],
 })
 export class TeamsModule {}

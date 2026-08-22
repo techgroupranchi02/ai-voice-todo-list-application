86c384761b27443fd3dd4682b2a18d8ee49ad664
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-22 11:31:39 +0530

    feat: create-task.dto.ts

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,24 +1,27 @@
-import { IsNotEmpty, IsOptional, IsDateString, IsIn } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
 
 export class CreateTaskDto {
   @IsNotEmpty()
-  @Column({ type: 'varchar', length: 255, nullable: false })
+  @IsString()
   title: string;
 
   @IsOptional()
-  @Column({ type: 'text', nullable: true })
-  description: string;
+  @IsString()
+  description?: string;
 
   @IsOptional()
-  @IsDateString()
-  @Column({ type: 'timestamp', nullable: true })
-  dueDate: Date;
+  @IsDate()
+  dueDate?: Date;
 
-  @IsIn([0, 1, 2]) // 0 = High, 1 = Medium, 2 = Low
-  @Column({ type: 'int', nullable: false, default: 0 })
+  @IsNotEmpty()
+  @IsNumber()
   priority: number;
 
   @IsOptional()
-  @Column({ type: 'varchar', length: 255, nullable: true })
-  category: string;
+  @IsString()
+  category?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  completed?: boolean;
 }

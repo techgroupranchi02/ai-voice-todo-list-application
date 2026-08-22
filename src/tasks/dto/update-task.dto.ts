1ff06e913afe1ccc6e074521384c2e0f367a7668
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-22 11:31:47 +0530

    feat: update-task.dto.ts

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskDto } from './create-task.dto';
+import { IsOptional, IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto {
+  @IsOptional()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsDate()
+  dueDate: Date;
+
+  @IsOptional()
+  @IsNumber()
+  priority: number;
+
+  @IsOptional()
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @IsBoolean()
+  completed: boolean;
+}

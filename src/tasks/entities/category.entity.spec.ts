c0339c72ed6c8d440bd4a5fd7d79b49e3d126488
Saurav kumar Mehta <techgroupranchi02@gmail.com>
2026-08-22 11:29:46 +0530

    feat: category.entity.spec.ts

--- src/tasks/entities/category.entity.spec.ts
+++ src/tasks/entities/category.entity.spec.ts
@@ -1,15 +1,15 @@
 import { Category } from './category.entity';
 
 describe('Category Entity', () => {
-  it('should be defined', () => {
-    expect(new Category()).toBeDefined();
+  it('should create a category entity instance', () => {
+    const category = new Category();
+    expect(category).toBeInstanceOf(Category);
   });
 
   it('should have required fields', () => {
     const category = new Category();
-    expect(category.id).toBeUndefined();
-    expect(category.name).toBeUndefined();
-    expect(category.created_at).toBeUndefined();
-    expect(category.updated_at).toBeUndefined();
+    category.name = 'Personal';
+
+    expect(category.name).toBe('Personal');
   });
 });
